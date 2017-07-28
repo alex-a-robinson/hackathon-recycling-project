d078a66157fb5e82789a83bb1dbd30faddbccc2b
begly <service.alex@begly.co.uk>
2017-07-28 14:57:14 +0100

    the best chagnes

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,12 @@
+var table;
+
 $(document).ready(function() {
 
   firebase.auth().onAuthStateChanged(user => {
     console.log(user);
     if (user) {
       window.user = user
+      table = $('#history').DataTable();
       firebase.database().ref(`/${user.uid}/history`).on('child_added', add_history);
     } else {
       firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
@@ -14,8 +17,9 @@ $(document).ready(function() {
 
 function add_history(snap) {
   if (!snap.val()) return;
+  var value = snap.val();
 
-  console.log(snap.val());
+  table.row.add([value.item_name, value.timestamp]).draw();
 }
 
 function add_new_item() {
